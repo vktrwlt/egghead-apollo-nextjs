56f3dcee84e62178d6c3ddc50781d11b2c2f8201
Victor Tsang <victorwltsang@gmail.com>
2023-02-20 14:34:09 -0800

    connect to FaunaDB and query data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
+import { useQuery, gql } from "@apollo/client";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+
+const GET_SHOP = gql`
+  query {
+    findShopByID(id: "357220645887541848") {
+      _id
+      description
+      name
+      ownerId
+      products {
+        _id
+      }
+    }
+  }
+`;
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SHOP);
+
+  console.log(data);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <p>Hello World</p>
