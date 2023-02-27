e238ad71370f9b62eb72a84bbf4c25af62258417
Victor Tsang <victorwltsang@gmail.com>
2023-02-26 20:13:52 -0800

    cart reducer

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-
+import { useContext } from "react";
+import { Context } from "../context";
 const navLinkStyle = `cursor-pointer inline-flex items-center px-1 pt-1 text-gray-500 hover:text-indigo-400`;
 
 const iconButtonStyle = `flex bg-white p-2 rounded-full text-gray-400 hover:text-indigo-400 border-2 border-gray-500 hover:border-indigo-400`;
@@ -9,6 +10,16 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Nav() {
   const { user, isLoading } = useUser();
   console.log("--->", user);
+
+  const { state } = useContext(Context as any);
+  console.log("--->", state);
+
+  const { cart } = state;
+  let itemCount = 0;
+
+  for (const [key, value] of Object.entries(cart)) {
+    itemCount = itemCount + cart[key].qty;
+  }
 
   return (
     <nav className="bg-white shadow">
@@ -32,7 +43,7 @@ export default function Nav() {
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-                {` ${3} Item(s)`}
+                {` ${itemCount} Item(s)`}
               </button>
             </Link>
             {!user ? (
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,3 +1,62 @@
+import { useContext } from "react";
+import { Context } from "../context";
+
 export default function Cart() {
-  return <div>Cart Page!</div>;
+  const { state, dispatch } = useContext(Context as any);
+  const { cart } = state;
+  let total = 0;
+
+  for (const [key, value] of Object.entries(cart)) {
+    total = total + cart[key].price * cart[key].qty;
+  }
+
+  return (
+    <div className="flex-1 px-4 py-6">
+      <ul role="list" className="my-6 divide-y divide-gray-200">
+        {Object.entries(cart).map(([key, value]) => {
+          return (
+            <li className="flex py-6">
+              <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md">
+                <img src={cart[key].imageUrl} className="object-cover object-center w-full h-full" />
+              </div>
+
+              <div className="flex flex-col flex-1 ml-4">
+                <div>
+                  <div className="flex justify-between text-base font-medium text-gray-900">
+                    <h3>{cart[key].name}</h3>
+                    <p className="ml-4">$ {cart[key].price}</p>
+                  </div>
+                  <p className="mt-1 text-sm text-gray-500">{cart[key].description}</p>
+                </div>
+                <div className="flex items-end justify-between flex-1 text-sm">
+                  <p className="text-gray-500">Qty {cart[key].qty}</p>
+
+                  <div className="flex">
+                    <button
+                      type="button"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                      onClick={() => {
+                        dispatch({ type: "REMOVE_FROM_CART", payload: cart[key] });
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="mt-6">
+        <div>Total $ {total}</div>
+        <a
+          href="#"
+          className="flex items-center justify-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md"
+        >
+          Checkout
+        </a>
+      </div>
+    </div>
+  );
 }
